364c02b1fd783307fb5b664c6e96b94e3c1577e9
Natalia <natpli@st.amu.edu.pl>
2021-04-29 22:53:29 +0200

    comicik natalia

--- tutorex/tutorex/settings.py
+++ tutorex/tutorex/settings.py
@@ -51,12 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tutorex.urls'
+ROOT_URLCONF = 'tutorex.urls' ###
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], ##
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') ###
