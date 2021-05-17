ccd2ea48f83f9deecf9b6244b3a2d48358af21b7
andrzej <andrzejjpreibisz@gmail.com>
2021-05-17 23:18:10 +0200

    Modified test cases - added secure=True to every request

--- users/tests.py
+++ users/tests.py
@@ -49,43 +49,43 @@ class BaseTest(TestCase):
 class RegisterTest(BaseTest):
     # view tests
     def test_view_page(self):
-        response = self.client.get(self.register_url)
+        response = self.client.get(self.register_url, secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup_form.html')
 
     def test_view_page_teacher(self):
-        response = self.client.get(self.register_url_teacher)
+        response = self.client.get(self.register_url_teacher, secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup_form_teacher.html')
 
     def test_view_page_parent(self):
-        response = self.client.get(self.register_url_parent)
+        response = self.client.get(self.register_url_parent, secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup_form_parent.html')
 
     def test_view_page_register_type(self):
-        response = self.client.get(self.register_url_type)
+        response = self.client.get(self.register_url_type, secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'register_type.html')
 
     # registration tests
     def test_register_user(self):
-        response = self.client.post(self.register_url, self.user, format='text/html')
+        response = self.client.post(self.register_url, self.user, format='text/html', secure=True)
         self.assertEqual(response.status_code, 302)
 
     def test_register_teacher(self):
-        response = self.client.post(self.register_url_teacher, self.user_teacher, format='text/html')
+        response = self.client.post(self.register_url_teacher, self.user_teacher, format='text/html', secure=True)
         self.assertEqual(response.status_code, 302)
 
     def test_register_parent(self):
-        response = self.client.post(self.register_url_parent, self.user_parent, format='text/html')
+        response = self.client.post(self.register_url_parent, self.user_parent, format='text/html', secure=True)
         self.assertEqual(response.status_code, 302)
 
 
 class LoginTest(BaseTest):
     # view tests
     def test_view_page(self):
-        response = self.client.get(self.login_url)
+        response = self.client.get(self.login_url, secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
@@ -101,7 +101,7 @@ class LoginTest(BaseTest):
         user.is_active = True
         user.save()
         response = self.client.post(self.login_url, {'email':'test@test.com',
-                                                'password':'1234'}, format='text/html')
+                                                'password':'1234'}, format='text/html', secure=True)
         self.assertEqual(response.status_code, 302)
 
     def test_login_unactive(self):
@@ -114,7 +114,7 @@ class LoginTest(BaseTest):
                                                 is_teacher=False)
         user.save()
         response = self.client.post(self.login_url, {'email':'test@test.com',
-                                                'password':'1234'}, format='text/html')
+                                                'password':'1234'}, format='text/html', secure=True)
         self.assertEqual(response.status_code, 200)
 
 
@@ -128,12 +128,12 @@ class LoginTest(BaseTest):
                                                 is_teacher=False)
         user.save()
         response = self.client.post(self.login_url, {'email':'test@test.com',
-                                                'password':'5678'}, format='text/html')
+                                                'password':'5678'}, format='text/html', secure=True)
         self.assertEqual(response.status_code, 200)
 
     def test_login_wrong_email(self):
         response = self.client.post(self.login_url, {'email':'test@example.com',
-                                                'password':'1234'}, format='text/html')
+                                                'password':'1234'}, format='text/html', secure=True)
         self.assertEqual(response.status_code, 200)
 
 
@@ -150,13 +150,13 @@ class ProfileTest(BaseTest):
         user.is_active = True
         user.save()
         self.client.post(self.login_url, {'email':'test@test.com',
-                                                'password':'1234'}, format='text/html')
+                                                'password':'1234'}, format='text/html', secure=True)
         return user
 
     # view tests
     def test_view_profile(self):
         user = self.set_up()
         user_id = user.id
-        response = self.client.get(reverse('user-edit-data', args=[user_id]))
+        response = self.client.get(reverse('user-edit-data', args=[user_id]), secure=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'account.html')
