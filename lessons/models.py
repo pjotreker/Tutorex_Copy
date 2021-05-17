de2697fd404019a9b98d8931361234c290a2c986
Marta Roszak <roszak999@gmail.com>
2021-05-17 21:45:12 +0200

    Classroom model v.1

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import BaseUser, TeacherProfile
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Lesson(models.Model):
+    pass
+
+
+class Classroom(models.Model):
+    subject = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, unique=True)
+    owner = models.ForeignKey(TeacherProfile, on_delete=models.CASCADE)
+    classroom_id = models.CharField(primary_key=True)
+    age_range = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)], blank=True)
+    time_frame = models.CharField(max_length=255)
+    lessons = models.ManyToManyField(Lesson)
+    students = models.ManyToManyField(BaseUser)
+
