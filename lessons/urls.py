31ad06c9afbe05ec640dbd2130c2e1adc63e7d11
Natalia <natpli@st.amu.edu.pl>
2021-05-23 17:27:30 +0200

    create show_classrooms

--- lessons/urls.py
+++ lessons/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('create-classroom/', lessons_views.CreateClassroom.as_view(), name="create-classroom"),
     path('classroom-created/<classroom_id>', lessons_views.ClassroomCreated.as_view(), name="classroom-created-success"),
     path('join-classroom/', lessons_views.JoinClassroom.as_view(), name="join_classroom"),
-
+    path('show-classrooms/', lessons_views.ShowClassrooms.as_view(), name="show-classrooms")
 ]
 
