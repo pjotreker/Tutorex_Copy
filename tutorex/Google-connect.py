794858bbfd95724176fb96dc349a34e560597eb2
Natalia <natpli@st.amu.edu.pl>
2021-05-31 21:04:42 +0200

    connect to GoogleCalendar API

--- tutorex/Google-connect.py
+++ tutorex/Google-connect.py
@@ -4,6 +4,10 @@ API_NAME='drive'
 API_VERSION='v3'
 SCOPES=['https://www.googleapis.com/auth/drive']
 
+API_NAME_CALENDAR='calendar'
+SCOPES_CALENDAR=['https://www.googleapis.com/auth/calendar']
+
 service=Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-print(dir(service))
+service_calendar=Create_Service(CLIENT_SECRET_FILE, API_NAME_CALENDAR, API_VERSION, SCOPES_CALENDAR)
+#print(dir(service_calendar))
