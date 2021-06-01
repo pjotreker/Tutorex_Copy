68ffee3d7f30982c6999d31434b3694f3a08ee4f
lila0073 <lila0073>
2021-06-01 12:33:02 +0200

    'commit-try'

--- tutorex/Google-connect.py
+++ tutorex/Google-connect.py
@@ -10,4 +10,3 @@ SCOPES_CALENDAR=['https://www.googleapis.com/auth/calendar']
 service=Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 service_calendar=Create_Service(CLIENT_SECRET_FILE, API_NAME_CALENDAR, API_VERSION, SCOPES_CALENDAR)
-#print(dir(service_calendar))
