84241feec8956aa8da9264b7f55b89925915aac0
Marta Roszak <roszak999@gmail.com>
2021-06-04 22:05:15 +0200

    [TUTX-122] create lesson

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
-from .models import BaseUser, Classroom
+from .models import BaseUser, Classroom, Lesson
 from datetime import date
 
 
@@ -29,3 +29,17 @@ class ModifyClassroomForm(forms.Form):
     # class Meta:
     #    model = Classroom
     #    fields = ["class_name", "subject", "age_range_min", "age_range_max", "time_frame_start", "time_frame_end"]
+
+
+class AddLessonForm(forms.ModelForm):
+    lesson_name = forms.CharField(max_length=100)
+    description = forms.CharField(required=False, max_length=255)
+    note = forms.CharField(required=False, max_length=1000)
+    date = forms.DateField(required=False, widget=forms.DateInput())
+    hour = forms.TimeField(required=False, input_formats=['%H:%M'], widget=forms.TimeInput())
+    lesson_done = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Lesson
+        fields = ["lesson_name", "description", "note", "date", "hour", "lesson_done"]
+
