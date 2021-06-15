663705898c4f32061c0e2d28099c8c32d1faf563
andrzej <andrzejjpreibisz@gmail.com>
2021-06-16 00:39:15 +0200

    Fix test

--- users/views.py
+++ users/views.py
@@ -574,9 +574,11 @@ class Contact(View):
     def get(self, request, *args, **kwargs):
         return render(request, "contact_us.html")
 
+
 class Terms(View):
     def get(self, request, *args, **kwargs):
         return render(request, "terms.html")        
+
 
 def get_user_notifications(request):
     user = request.user
